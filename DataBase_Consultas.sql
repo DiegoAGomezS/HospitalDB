e679a7a28acd5f57aeda939a0b995ce581def645
DiegoAGomezS <dagomez@uamv.edu.ni>
2026-06-04 07:56:35 -0600

    DataBaseCreation

--- DataBase_Consultas.sql
+++ DataBase_Consultas.sql
@@ -1,2 +1,17 @@
 use master
 go
+
+-- 1. Crear la base de datos "HospitalDB".
+IF EXISTS (SELECT name FROM sys.databases WHERE name = N'HospitalDB')
+BEGIN
+    ALTER DATABASE HospitalDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE HospitalDB;
+END
+GO
+
+CREATE DATABASE HospitalDB;
+GO
+
+USE HospitalDB;
+GO
+
