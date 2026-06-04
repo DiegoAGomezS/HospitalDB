8ce1a7f955038f59061c3e7c6494a88c884983fe
DiegoAGomezS <dagomez@uamv.edu.ni>
2026-06-04 07:58:53 -0600

    Esquematizacion

--- DataBase_Consultas.sql
+++ DataBase_Consultas.sql
@@ -12,6 +12,23 @@ GO
 CREATE DATABASE HospitalDB;
 GO
 
+-- 2. Mostrar todas las bases de datos en el sistema
+
+select * from sys.databases;
+GO
+
+-- 3. Seleccionar la base de datos "HospitalDB" para trabajar en ella.
 USE HospitalDB;
 GO
+
+-- 3.5. Discutir los esquemas a crear para dividir la base de datos en áreas funcionales.
+
+Create schema Personal -- Información relacionada a los Médicos y sus especialidades.
+go
+
+Create schema Visitas -- Información relacionada a los pacientes, como info personal, tratamientos y medicamentos.
+go
+
+Create schema Agendas -- Información relacionada a las citas médicas y Habitaciones en las que se dieron dichas citas.
+go
 
